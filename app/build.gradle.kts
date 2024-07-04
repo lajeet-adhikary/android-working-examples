90ce07152c381d49fb978991179cac4f2b4beee7
lajeet-pc <lajeet.adhikary123@gmail.com>
2024-07-04 17:32:53 +0530

    added dependencies to run the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbranins.kotlin.kapt)
+    alias(libs.plugins.hilt.android.plugin)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
-    namespace = "com.lajeet.workingexamples"
+    namespace = "com.lajeet.practicecurrencyexchange"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.lajeet.workingexamples"
+        applicationId = "com.lajeet.practicecurrencyexchange"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -40,11 +40,12 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/gradle/incremental.annotation.processors"
         }
     }
 }
@@ -52,18 +53,45 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.lifecycle.vm)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //Dagger Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.logging.interceptor)
+
+    //Networking + Coroutines
+    implementation(libs.coroutines)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+
+    //Background Work
+    implementation(libs.work.manager)
+
+    //Compose + support
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.material)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.preview.support)
+    debugImplementation(libs.compose.preview.support.debug)
+    implementation(libs.compose.activity)
+    implementation(libs.compose.viewmodel)
+    implementation(libs.compose.livedata)
+    implementation(libs.compose.nav)
+    implementation(libs.compose.hilt.nav)
+
+    //Room
+    implementation(libs.room.db)
+    kapt(libs.room.db.compiler)
+    implementation(libs.room.ktx)
+    kapt(libs.room.ktx.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.android.plugin) apply false
+    alias(libs.plugins.jetbranins.kotlin.kapt) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }
